1c146dc9e0701cbb0b286d9dada4ce5b05ab37b0
Belal Sejouk <b.sejouk@gmail.com>
2026-07-03 01:19:53 +0200

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eclatdusud.fr',
+  site: "https://eclatdesud.fr",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     sitemap(),
-  ]
+  ],
 });
